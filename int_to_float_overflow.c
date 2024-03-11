75d8c94f90cfca99ac8864c3f79a8244df402c9d
escobaradelaida <adelaidaescobar@oakland.edu>
2024-03-11 18:37:49 -0400

    first commit

---
+++ int_to_float_overflow.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 0;
+  int j = (int)(float)i;
+  /*
+  Fill in the blank below
+  Find the smallest positive integer i such that i != j where j = (int)(float)i;
+
+  Hint: Use a while loop.
+  The sample solution only has four lines.
+  */
+
+
+
+
+  printf("i=%d\nj=%d\n", i, j);
+}
